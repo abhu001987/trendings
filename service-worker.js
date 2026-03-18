e85c54532ad303cb7046aefe5452e265eff7496e
abhu001987 <Abhikr18996@gmail.com>
2026-03-18 13:19:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,27 @@
-self.addEventListener("install",event=>{
-self.skipWaiting();
+const CACHE_NAME = "treedell-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("activate",event=>{
-event.waitUntil(self.clients.claim());
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch",event=>{
-event.respondWith(
-fetch(event.request).catch(()=>{
-return caches.match(event.request);
-})
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
